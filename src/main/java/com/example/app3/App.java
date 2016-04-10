4ee6ff04968997b7e0300711fecab1bf7928f77e
hshs <freegate22@hotmail.com>
2016-04-10 22:54:18 +0900

    spring boot jdbc

---
+++ src/main/java/com/example/app3/App.java
@@ -0,0 +1,41 @@
+package com.example.app3;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import com.example.app2.domain.Customer;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class App implements CommandLineRunner{
+	
+	@Autowired
+	NamedParameterJdbcTemplate jdbcTemplate;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
+		SqlParameterSource param = new MapSqlParameterSource()
+				.addValue("id", 1);
+		Customer result = jdbcTemplate.queryForObject(sql, param, 
+				(rs, rowNum) -> new Customer(rs.getInt("id"), 
+						rs.getString("first_name"), rs.getString("last_name")));
+		System.out.println("result =" + result);
+		
+	}
+
+}
