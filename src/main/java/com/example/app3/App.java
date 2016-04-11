e032c20793e54be6af1b2439a17c957764f51cad
hshs <freegate22@hotmail.com>
2016-04-11 22:50:25 +0900

    repository 추가

--- src/main/java/com/example/app3/App.java
+++ src/main/java/com/example/app3/App.java
@@ -1,26 +1,20 @@
 package com.example.app3;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import com.example.app2.domain.Customer;
+import com.example.app3.repository.CustomerRepository;
 
 @EnableAutoConfiguration
 @ComponentScan
 public class App implements CommandLineRunner{
 	
 	@Autowired
-	NamedParameterJdbcTemplate jdbcTemplate;
+	CustomerRepository customerRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
@@ -28,13 +22,12 @@ public class App implements CommandLineRunner{
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("id", 1);
-		Customer result = jdbcTemplate.queryForObject(sql, param, 
-				(rs, rowNum) -> new Customer(rs.getInt("id"), 
-						rs.getString("first_name"), rs.getString("last_name")));
-		System.out.println("result =" + result);
+		//데이터 추가
+		Customer created = customerRepository.save(new Customer(null, "Hidetosh","Dekisuki"));
+		System.out.println(created+" is created!");
+		customerRepository.findAll().forEach(System.out::println);
+		
+		
 		
 	}
 
